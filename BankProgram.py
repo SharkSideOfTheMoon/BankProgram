d4b1f46b64e93e94a0e2ec17e84c351cbe36d1fa
SharkSideOfTheMoon <patrick@patrickrobertsuk.com>
2020-04-25 14:55:35 +0100

    Added Transaction History
    You can now view your account's transaction history, and there is now a function to add one to a data table that was added to the database.

--- BankProgram.py
+++ BankProgram.py
@@ -1,6 +1,7 @@
 import sqlite3
 import tkinter
 import math
+import datetime
 
 def Startup():
     window = tkinter.Tk()
@@ -103,6 +104,7 @@ def Startup():
         global var_current_account_balance_interest_added
         global var_current_account_generated_interest
         global balance_message
+        
         window.destroy()
         window2 = tkinter.Tk()
         window2.title("Banking Program")
@@ -111,7 +113,7 @@ def Startup():
         GetVariablesForSetup()
         print(var_current_account_generated_interest)
         var_current_account_balance_interest_added = round_down((var_current_account_balance * 1.002), 2)
-        var_current_account_generated_interest = var_current_account_generated_interest + (var_current_account_balance_interest_added - var_current_account_balance)
+        var_current_account_generated_interest = round_down(var_current_account_generated_interest + (var_current_account_balance_interest_added - var_current_account_balance), 2)
         logged_in_message = "You are logged in as: {}          Your UID is {}".format(var_current_user_username, var_UID)
         balance_message_label_text = tkinter.StringVar()
         
@@ -139,7 +141,61 @@ def Startup():
         enter_ammount_to_exchange_label = tkinter.Label(window2, text = "Please enter the amount in £ you would like to transfer (Enter only a number)").pack()
         enter_ammount_to_exchange_entrybox = tkinter.Entry(window2)
         enter_ammount_to_exchange_entrybox.pack()
-
+        
+        def ShowHistory():
+            def CountLines():
+                i = 0
+                with sqlite3.connect('LoginDataBase.db') as DbConnect:
+                    c = DbConnect.cursor()
+                    var_select = []
+                    var_select.append(var_UID)
+                    var_select.append(var_current_user_username)
+                    var_select.append(var_UID)
+                    var_select.append(var_current_user_username)
+                    sql_statement_find_history = 'SELECT * FROM TransactionHistoryTable WHERE User_Number_1 = ? AND User_Name_1 = ? OR User_Number_2 = ? AND Username_2 = ?';
+                    c.execute(sql_statement_find_history, var_select)
+                    result_history = c.fetchall()
+                    print(result_history)                
+                    for row in result_history:
+                        i = i + 1
+                return i
+            hw = tkinter.Tk()
+            scrollbar = tkinter.Scrollbar(hw)
+            hw.title("Transaction History")
+            hw.geometry("960x540")
+            number_of_lines = CountLines()
+            hw_text_box = tkinter.Text(hw, height = number_of_lines, width = 120)
+            scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+            hw_text_box.pack(side=tkinter.LEFT, fill=tkinter.Y)
+            scrollbar.config(command=hw_text_box.yview)
+            hw_text_box.config(yscrollcommand=scrollbar.set)
+            
+            with sqlite3.connect('LoginDataBase.db') as DbConnect:
+                    c = DbConnect.cursor()
+                    var_select = []
+                    var_select.append(var_UID)
+                    var_select.append(var_current_user_username)
+                    var_select.append(var_UID)
+                    var_select.append(var_current_user_username)
+                    sql_statement_find_history = 'SELECT * FROM TransactionHistoryTable WHERE User_Number_1 = ? AND User_Name_1 = ? OR User_Number_2 = ? AND Username_2 = ?';
+                    c.execute(sql_statement_find_history, var_select)
+                    result_history = c.fetchall()
+                    print(result_history)                
+                    for row in result_history:
+                        str_message = "Date: {} || {} ---> {} || £{}\n".format(row[0], row[2], row[4], row[5])
+                        hw_text_box.insert(tkinter.END, str_message)
+            
+        def RecordTransaction(UID, current_user, UID2, current_user_2, payment):
+            date_not_clipped = str(datetime.datetime.now())
+            date_clipped_array = date_not_clipped.split('.')
+            date_clipped = date_clipped_array[0]
+            print(date_clipped)
+            with sqlite3.connect('LoginDataBase.db') as DbConnect:
+                c = DbConnect.cursor()
+                c.execute("INSERT INTO TransactionHistoryTable (Date, User_Number_1, User_Name_1, User_Number_2, Username_2, Payment_Amount) VALUES (?,?,?,?,?,?)", [date_clipped, UID, current_user, UID2, current_user_2, payment])
+                DbConnect.commit()
+            
+            
         def PayUser(tempvar_UID ,account, ammount):
             global var_current_account_balance_interest_added
             global balance_message
@@ -165,6 +221,7 @@ def Startup():
                     print("Second User data " + str(second_user_balance))
                     second_user_balance = second_user_balance + ammount
                     var_current_account_balance_interest_added = tempbalance
+                    var_current_account_balance_interest_added = round_down(var_current_account_balance_interest_added, 2)
                     sql_update_statement_transaction_give = "UPDATE BankAccountDataTable SET Account_Balance_Current = ? WHERE User_Number = ? AND Username = ?"
                     sql_update_statement_transaction_recieve = "UPDATE BankAccountDataTable SET Account_Balance_Current = ? WHERE User_Number = ? AND Username = ?"
                     Values2 = (var_current_account_balance_interest_added, var_UID, var_current_user_username)
@@ -173,6 +230,9 @@ def Startup():
                     c.execute(sql_update_statement_transaction_recieve, Values3)
                     balance_message = "Your Current Balance is: £{}".format(var_current_account_balance_interest_added)
                     DbConnect.commit()
+                RecordTransaction(var_UID, var_current_user_username, tempvar_UID, account, ammount)
+                    
+                    
                     
             else:
                 FailWindowFunc("Not enough money")
@@ -199,23 +259,34 @@ def Startup():
         def GetInfo():
             recieving_account = enter_user_for_transaction_entrybox.get()
             parts_of_full_input = recieving_account.split('_')
-            recieving_account_UID = int(parts_of_full_input[0])
-            print(recieving_account_UID)
-            recieving_account_parsed_name = parts_of_full_input[1]
-            print(recieving_account_parsed_name)
             try:
-                money_to_transfer = float(enter_ammount_to_exchange_entrybox.get())
-                if(money_to_transfer > 0):
-                    CheckUserExists(recieving_account_UID, recieving_account_parsed_name, money_to_transfer)
-                    RefreshPage()
+                recieving_account_UID = int(parts_of_full_input[0])
+                print(recieving_account_UID)
+                recieving_account_parsed_name = parts_of_full_input[1]
+                print(recieving_account_parsed_name)
+                if(recieving_account_UID != var_UID):
+                    try:
+                        money_to_transfer = float(enter_ammount_to_exchange_entrybox.get())
+                        if(money_to_transfer > 0):
+                            CheckUserExists(recieving_account_UID, recieving_account_parsed_name, money_to_transfer)
+                            RefreshPage()
+                        else:
+                            FailWindowFunc("The transfer amount was less than or equal to 0")
+                    except ValueError as e:
+                        FailWindowFunc("That was not a number")
+                        #enter_ammount_to_exchange_entrybox.delete(0, "end")
                 else:
-                    FailWindowFunc("The transfer amount was less than or equal to 0")
+                    FailWindowFunc("You cannot transfer money to yourself!")
             except ValueError as e:
-                FailWindowFunc("That was not a number")
-                #enter_ammount_to_exchange_entrybox.delete(0, "end")
+                FailWindowFunc("That was not a valid username formatted 'UID_Username'!")
+            
 
         pay_user_button = tkinter.Button(window2, command = GetInfo, text = "Pay User")
         pay_user_button.pack()
+        space_label = tkinter.Label(window2).pack()
+        check_history_transaction_label = tkinter.Label(window2, text = "Press History to View Past Transactions").pack()
+        check_history_transaction_button = tkinter.Button(window2, command = ShowHistory, text = "History")
+        check_history_transaction_button.pack()
 
         window2.mainloop()
  
